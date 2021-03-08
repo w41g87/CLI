bfde877fa9dfe4e0fc74a51667e7e365035171ba
Yanjun Jin <jin327@data.cs.purdue.edu>
2021-03-07 20:09:33 -0500

    \Total:                   0.0 of 70.0\

--- command.cc
+++ command.cc
@@ -161,7 +161,7 @@ void Command::execute() {
         }
         if (++i > 1) {
             dup2(fdpipe[0], 0);
-            close(fdpipe[0]);
+            //close(fdpipe[0]);
         }
         if (i == _simpleCommands.size()) {
             if (_outFile) {
@@ -171,7 +171,7 @@ void Command::execute() {
             else dup2(defaultout, 1);
         } else {
             dup2(fdpipe[1], 1);
-            close(fdpipe[1]);
+            //close(fdpipe[1]);
         }
 
         printf("Forking...\n");
