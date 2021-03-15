90115a6270bb92b73263f529d781dd58868aeeeb
Yanjun Jin <jin327@data.cs.purdue.edu>
2021-03-14 23:38:48 -0400

    \Grade Report for Lab 3 Testall\

--- lex.yy.cc
+++ lex.yy.cc
@@ -524,7 +524,7 @@ std::string removeE(char * s) {
   for(int i = 0; i < strlen(s); i++) {
     if ((i > 0 && s[i - 1] == '\\') || s[i] != '\"' || s[i] != '\\') {
       str.push_back(s[i]);
-      printf("s[i]");
+      printf("%c", s[i]);
     }
   }
   return str;
