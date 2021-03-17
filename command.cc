ff8759df16620991d1f4f71cca91130914eebcb8
Yanjun Jin <jin327@data.cs.purdue.edu>
2021-03-17 12:44:22 -0400

    \Grade Report for Lab 3 Testall\

--- command.cc
+++ command.cc
@@ -272,7 +272,7 @@ void Command::execute() {
             if (_pid == 0) {
                 //Child
                 const char * cmd = simpleCommand->_arguments.front()->c_str();
-                const char ** args = (const char**)simpleCommand->toString();
+                char ** args = simpleCommand->toString();
                 //close file descriptors that are not needed
                 // close(fdpipe[0]);
                 // close(fdpipe[1]);
