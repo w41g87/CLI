4a8e7e8e8dba1f4a4b6bffbba28c2c532366ea8b
Yanjun Jin <jin327@data.cs.purdue.edu>
2021-03-17 13:43:06 -0400

    \Grade Report for Lab 3 Testall\

--- command.cc
+++ command.cc
@@ -154,7 +154,7 @@ void Command::execute() {
         }
 
         if (!strcmp(cmd, "setenv")) {
-            char ** arg = _simpleCommands.front()->toString();
+            const char ** arg = _simpleCommands.front()->toString();
             while(arg[i++]);
             if (i != 4) cout << "setenv: argument number mismatch." << endl;
             else if (setenv(arg[1], arg[2], 1) != 0) perror("setenv");
@@ -162,7 +162,7 @@ void Command::execute() {
             return;
         }
         if (!strcmp(cmd, "unsetenv")) {
-            char ** arg = _simpleCommands.front()->toString();
+            const char ** arg = _simpleCommands.front()->toString();
             while(arg[i++]);
             if (i != 3) cout << "unsetenv: argument number mismatch." << endl;
             else if (unsetenv(arg[1]) != 0) perror("unsetenv");
@@ -170,7 +170,7 @@ void Command::execute() {
             return;
         }
         if (!strcmp(cmd, "cd")) {
-            char ** arg = _simpleCommands.front()->toString();
+            const char ** arg = _simpleCommands.front()->toString();
             while(arg[i++]);
             //printf("%d\n", i);
             if (i > 3) cout << "cd: too many arguments." << endl;
@@ -180,7 +180,7 @@ void Command::execute() {
             return;
         }
         if (!strcmp(cmd, "source")) {
-            char ** arg = _simpleCommands.front()->toString();
+            const char ** arg = _simpleCommands.front()->toString();
             while(arg[i++]);
             if (i != 3) cout << "source: argument number mismatch." << endl;
             else {
