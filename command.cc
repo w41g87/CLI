918a62a15058ad73af45e9fa2a2141dc3a221bde
Yanjun Jin <jin327@data.cs.purdue.edu>
2021-03-17 14:18:31 -0400

    \Grade Report for Lab 3 Testall\

--- command.cc
+++ command.cc
@@ -111,8 +111,8 @@ void Command::print() {
 
 void Command::embedDest(char** args) {
     int i = 0;
-    while(args[i++] != 0) free(args[i]);
-    //free(args);
+    //while(args[i++] != 0) free(args[i]);
+    free(args);
     clear();
     Shell::prompt();
 }
