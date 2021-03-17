336db657f1273adbb470d65dbf1eeb5e13e76da7
Yanjun Jin <jin327@data.cs.purdue.edu>
2021-03-17 16:15:56 -0400

    \Part 1 Total:            11.0 of 20.0\

--- command.cc
+++ command.cc
@@ -164,10 +164,6 @@ void Command::execute() {
 
         if (!strcmp(cmd, "setenv")) {
             char ** arg = _simpleCommands.front()->toString();
-            while(arg[i++] != 0) {
-                cout << arg[i - 1] << endl;
-            }
-            cout << i << endl;
             if (i != 4) cout << "setenv: argument number mismatch." << endl;
             else if (setenv(arg[1], arg[2], 1) != 0) perror("setenv");
             embedDest(arg);
