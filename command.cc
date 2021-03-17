57ebc784cb839720c118062b0079ed00624917bc
Yanjun Jin <jin327@data.cs.purdue.edu>
2021-03-17 14:27:38 -0400

    \Grade Report for Lab 3 Testall\

--- command.cc
+++ command.cc
@@ -111,7 +111,7 @@ void Command::print() {
 
 void Command::embedDest(char** args) {
     int i = 0;
-    //while(args[i++] != 0) free(args[i]);
+    while(args[i++] != 0) free(args[i]);
     free(args);
     clear();
     Shell::prompt();
@@ -295,7 +295,7 @@ void Command::execute() {
                 exit( 2 );
             }
             // while(args[i++] != 0) delete args[i];
-            embedDest(args);
+            free(args);
         }
         //printf("pid: %d", _pid);
         if (!_background) waitpid( _pid, 0, 0 );
