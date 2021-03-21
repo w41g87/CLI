81311813b68cc3f6a613f51301028b8b8ad3a38a
Yanjun Jin <jin327@data.cs.purdue.edu>
2021-03-21 16:58:45 -0400

    \Part 1 Total:            11.0 of 20.0\

--- shell.cc
+++ shell.cc
@@ -9,6 +9,7 @@ int yyparse(void);
 void swtchBfr(char*);
 void initBfr();
 void flushBfr();
+extern char ** history;
 
 void destroy(char** args) {
     int i = 0;
@@ -71,6 +72,8 @@ int main(int argc, char* argv[], char* envp[]) {
     free(input);
     exit(0);
   }
+  //initialize history
+  history = (char**) calloc(8, sizeof(char*));
 
   // store argv[0];
   Shell::argv = argv[0];
