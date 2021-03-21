28868f370a4dc73b1e3ec26eb3f74ac2c9de3fe3
Yanjun Jin <jin327@data.cs.purdue.edu>
2021-03-21 06:27:55 -0400

    \Part 1 Total:            11.0 of 20.0\

--- command.cc
+++ command.cc
@@ -146,7 +146,6 @@ void Command::execute() {
         free(Shell::lstArg);
         Shell::lstArg = (char *)calloc(strlen(lstArg) + 1, sizeof(char));
         strcpy(Shell::lstArg, lstArg);
-        printf("%s recorded\n", Shell::lstArg);
     }
 
     // Print contents of Command data structure
