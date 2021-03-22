29e8f2b09f408210c7f2d6084c0c98343790f056
Yanjun Jin <jin327@data.cs.purdue.edu>
2021-03-21 22:43:51 -0400

    \Total:                   46.5 of 70.0\

--- shell.cc
+++ shell.cc
@@ -32,7 +32,8 @@ void Shell::termination(int signum) {
 
 void Shell::elimination(int signum) {
   int r = 0;
-  int e = waitpid(0, &r, 0);
+  int e = waitpid(-1, &r, 0);
+  printf("exit: %d", WEXITSTATUS(r));
   if (e == -1) Shell::lstRtn = WEXITSTATUS(r);
   else {
     if (isatty(0)) printf("%d exited\n", e);
